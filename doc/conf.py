3316f56e8589c58109931af59d23e25a182c6a5b
Daren Thomas <thomas@arch.ethz.ch>
2017-02-01 11:56:01 +0100

    readthedocs blue theme

--- doc/conf.py
+++ doc/conf.py
@@ -86,6 +86,9 @@ todo_include_todos = False
 #
 html_theme = 'alabaster'
 
+# this should make the style the blue read-the-docs style
+html_style = '/default.css'
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
